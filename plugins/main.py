05841310f5c1d5904eca029449e16db9aa01286e
ALUVALA EDIGA HARSHA VARDHAN GOUD <95118956+Aluval@users.noreply.github.com>
2023-05-30 14:30:04 +0530

    Update main.py

--- plugins/main.py
+++ plugins/main.py
@@ -9,8 +9,8 @@ from helper.text import txt
 async def start_message(bot, message):
     await add_user(bot, message)    
     button=InlineKeyboardMarkup([[
-        InlineKeyboardButton("🍁 ꜱᴜᴩᴩᴏʀᴛ", url="https://t.me/BETA_SUPPORT"),
-        InlineKeyboardButton("📯 ᴜᴩᴅᴀᴛᴇꜱ", url="https://t.me/Beta_BoTZ")
+        InlineKeyboardButton("🎬 𝐂𝐇𝐀𝐍𝐍𝐄𝐋", url="https://t.me/BETA_SUPPORT"),
+        InlineKeyboardButton("📢 𝐔𝐏𝐃𝐀𝐓𝐄𝐒", url="https://t.me/Beta_BoTZ")
         ],[            
         InlineKeyboardButton("ℹ️ ʜᴇʟᴩ", callback_data="help"),
         InlineKeyboardButton("📡 ᴀʙᴏᴜᴛ", callback_data="about") 
